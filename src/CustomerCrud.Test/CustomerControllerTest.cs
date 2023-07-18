71bc7194bba688808d00af9e822e66574cbd481d
Priscila <priscilasartori2@gmail.com>
2023-07-18 18:55:17 -0300

    feat: Crie o método GetAll

--- src/CustomerCrud.Test/CustomerControllerTest.cs
+++ src/CustomerCrud.Test/CustomerControllerTest.cs
@@ -6,49 +6,58 @@ namespace CustomerCrud.Test;
 
 public class CustomersControllerTest : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly HttpClient _client;
-    private readonly Mock<ICustomerRepository> _repositoryMock;
+  private readonly HttpClient _client;
+  private readonly Mock<ICustomerRepository> _repositoryMock;
 
-    public CustomersControllerTest(WebApplicationFactory<Program> factory)
+  public CustomersControllerTest(WebApplicationFactory<Program> factory)
+  {
+    _repositoryMock = new Mock<ICustomerRepository>();
+
+    _client = factory.WithWebHostBuilder(builder =>
     {
-        _repositoryMock = new Mock<ICustomerRepository>();
+      builder.ConfigureServices(services =>
+          {
+            services.AddScoped<ICustomerRepository>(st => _repositoryMock.Object);
+          });
+    }).CreateClient();
+  }
 
-        _client = factory.WithWebHostBuilder(builder =>
-        {
-            builder.ConfigureServices(services =>
-            {
-                services.AddScoped<ICustomerRepository>(st => _repositoryMock.Object);
-            });
-        }).CreateClient();
-    }
+  [Fact]
+  public async Task GetAllTest()
+  {
+    var customers = AutoFaker.Generate<Customer>(3);
+    _repositoryMock.Setup(r => r.GetAll()).Returns(customers);
 
-    [Fact]
-    public async Task GetAllTest()
-    {
-        throw new NotImplementedException();
-    }
+    var response = await _client.GetAsync("/customers");
+    var content = await response.Content.ReadFromJsonAsync<IEnumerable<Customer>>();
 
-    [Fact]
-    public async Task GetByIdTest()
-    {
-        throw new NotImplementedException();
-    }
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+    content.Should().BeEquivalentTo(customers);
 
-    [Fact]
-    public async Task CreateTest()
-    {
-        throw new NotImplementedException();
-    }
+    _repositoryMock.Verify(r => r.GetAll(), Times.Once);
+  }
 
-    [Fact]
-    public async Task UpdateTest()
-    {
-        throw new NotImplementedException();
-    }
+  [Fact]
+  public async Task GetByIdTest()
+  {
+    throw new NotImplementedException();
+  }
 
-    [Fact]
-    public async Task DeleteTest()
-    {
-        throw new NotImplementedException();
-    }
+  [Fact]
+  public async Task CreateTest()
+  {
+    throw new NotImplementedException();
+  }
+
+  [Fact]
+  public async Task UpdateTest()
+  {
+    throw new NotImplementedException();
+  }
+
+  [Fact]
+  public async Task DeleteTest()
+  {
+    throw new NotImplementedException();
+  }
 }
--- src/CustomerCrud/Controllers/CustomerController.cs
+++ src/CustomerCrud/Controllers/CustomerController.cs
@@ -1,3 +1,14 @@
+// O método GetAll deve ser acessível por uma requisição GET na rota "/controller"
+// Como a rota usada é a mesma que a rota base, apenas é necessário utilizar neste método o atributo que represente o verbo HTTP correto, sem nenhum parâmetro a mais.
+// O método GetAll deve responder com um ActionResult (ou derivado) contendo o status 200 (Ok), caso a leitura ocorra corretamente
+// A resposta do método GetAll deve retornar em seu corpo todas as entradas do banco
+// O método GetAllTest deve conter os testes para o método GetAll:
+// Uma chamada GET para a rota "/customers" utilizando o _client
+// Um mock do método GetAll() do repositório, configurado usando o _repositoryMock
+// Uma verificação de que a resposta retornada pela chamada ao cliente é do tipo 200 (Ok)
+// Uma verificação de que o conteúdo da resposta é equivalente ao objeto retornado pelo mock
+// Uma verificação de que o método mockado foi chamado uma única vez
+
 using Microsoft.AspNetCore.Mvc;
 using CustomerCrud.Core;
 using CustomerCrud.Requests;
@@ -5,6 +16,21 @@ using CustomerCrud.Repositories;
 
 namespace CustomerCrud.Controllers;
 
-public class CustomerController
+[ApiController]
+[Route("customers")]
+public class CustomerController : ControllerBase
 {
+  private readonly ICustomerRepository _repository;
+
+  public CustomerController(ICustomerRepository repository)
+  {
+    _repository = repository;
+  }
+
+  [HttpGet]
+  public ActionResult GetAll()
+  {
+    var customers = _repository.GetAll();
+    return Ok(customers);
+  }
 }
